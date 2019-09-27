8834a84fdb56b158557a0aa118244162d59f9524
YanJK <892225778@qq.com>
2019-09-27 14:02:32 +0800

    上传资源文件

---
+++ app/src/main/java/com/android/wanandroid/module/home/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.android.wanandroid.module.home;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.wanandroid.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_home);
+   }
+}
---
+++ app/src/main/java/com/android/wanandroid/module/welcome/WelcomeActivity.java
@@ -0,0 +1,14 @@
+package com.android.wanandroid.module.welcome;
+
+import com.android.mymvp.base.BaseActivity;
+import com.android.wanandroid.R;
+
+public class WelcomeActivity extends BaseActivity {
+
+   @Override
+   public int initLayout() {
+      return R.layout.activity_welcome;
+   }
+
+
+}
