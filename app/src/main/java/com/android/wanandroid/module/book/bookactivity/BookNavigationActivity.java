2573553a0e8d3f547b0e8cf4fd7bb5e92fd939f0
YanJK <892225778@qq.com>
2019-10-06 18:54:42 +0800

    更改了web的加载设置

--- app/src/main/java/com/android/wanandroid/module/book/bookactivity/BookNavigationActivity.java
+++ app/src/main/java/com/android/wanandroid/module/book/bookactivity/BookNavigationActivity.java
@@ -1,7 +1,7 @@
 package com.android.wanandroid.module.book.bookactivity;
 
 import android.content.Intent;
-import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -9,7 +9,6 @@ import com.android.mymvp.base.BaseActivity;
 import com.android.wanandroid.R;
 
 import butterknife.BindView;
-import butterknife.ButterKnife;
 
 public class BookNavigationActivity extends BaseActivity {
 
@@ -31,6 +30,28 @@ public class BookNavigationActivity extends BaseActivity {
         String link = intent.getStringExtra("link");
         //设置在webview 上
         booknavWeb.setWebViewClient(new WebViewClient());
+        WebSettings settings = booknavWeb.getSettings();
+        settings.setJavaScriptEnabled(true);//js支持
+        settings.setJavaScriptCanOpenWindowsAutomatically(true);//js支持
+        settings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
+        settings.setAllowFileAccess(true);
+        settings.setUseWideViewPort(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDisplayZoomControls(false);
+        settings.setBuiltInZoomControls(true);
+        settings.setSupportMultipleWindows(true);
+        settings.setAppCacheEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAppCacheMaxSize(Long.MAX_VALUE);
+        settings.setPluginState(WebSettings.PluginState.ON_DEMAND);
+        settings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        settings.setLoadWithOverviewMode(true);
+        settings.setGeolocationEnabled(true);
+        settings.setUserAgentString(settings.getUserAgentString());
+        settings.setAppCacheEnabled(true);
+        settings.setDatabaseEnabled(true);
+        settings.setMediaPlaybackRequiresUserGesture(false);
+
         booknavWeb.loadUrl(link);
     }
 }
