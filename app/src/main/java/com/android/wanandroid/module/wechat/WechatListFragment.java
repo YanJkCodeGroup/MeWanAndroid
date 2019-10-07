1c408e389214439248672116679af2217088739b
zyqs <844252650@qq.com>
2019-10-07 21:06:16 +0800

    公众号  one

--- app/src/main/java/com/android/wanandroid/module/wechat/WechatListFragment.java
+++ app/src/main/java/com/android/wanandroid/module/wechat/WechatListFragment.java
@@ -1,18 +1,27 @@
 package com.android.wanandroid.module.wechat;
 
+import android.content.Intent;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.loadingview.LoadingView;
 import com.android.mymvp.base.BaseMvpFragment;
+import com.android.utils.log.LogUtil;
 import com.android.wanandroid.Contract;
 import com.android.wanandroid.R;
+import com.android.wanandroid.module.book.bookactivity.BookNavigationActivity;
 import com.android.wanandroid.module.wechat.wechatadapter.WechatListsAdapter;
 import com.android.wanandroid.module.wechat.wehcatpresetner.WechatListPresenter;
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.listener.OnRefreshLoadMoreListener;
 
 import java.util.List;
 
@@ -21,8 +30,10 @@ import butterknife.BindView;
 public class WechatListFragment extends BaseMvpFragment<Contract.WechatlistPresenter> implements Contract.WechatlistView {
     @BindView(R.id.wechatlist_rec)
     RecyclerView wechatlistRec;
+    @BindView(R.id.smart_refresh)
+    SmartRefreshLayout smartRefresh;
     private int id;
-    private int page=1;
+    private int page = 1;
 
     private WechatListsAdapter wechatListsAdapter;
 
@@ -33,16 +44,39 @@ public class WechatListFragment extends BaseMvpFragment<Contract.WechatlistPrese
 
     @Override
     public void succeed(WechatListBean wechatList) {
-        List<WechatListBean.DatasBean> datas = wechatList.getDatas();
-        wechatListsAdapter = new WechatListsAdapter(datas, getActivity());
-        wechatlistRec.setAdapter(wechatListsAdapter);
-        wechatListsAdapter.notifyDataSetChanged();
+        if (page == 1) {
+            wechatListsAdapter.initData(wechatList.getDatas());
+        } else {
+            wechatListsAdapter.addData(wechatList.getDatas());
+        }
+        hideLoading();
+        smartRefresh.finishLoadMore();
+        smartRefresh.finishRefresh();
+        wechatListsAdapter.setOnClickListener(new WechatListsAdapter.OnClickListener() {
+            @Override
+            public void onClick(View v, int position) {
+                Intent intent = new Intent(mActivity, BookNavigationActivity.class);
+                intent.putExtra("link",wechatList.getDatas().get(position).getLink());
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
     public void fail(String error) {
         Log.d("booknav", "fail: " + error);
         Toast.makeText(mContext, "错误为:" + error, Toast.LENGTH_SHORT).show();
+        LogUtil.d(error);
+        hideLoading();
+        LoadingError(error, new LoadingView.RetryCallBack() {
+            @Override
+            public void onRetry() {
+                initData();
+                showLoading();
+            }
+        });
+        smartRefresh.finishLoadMore();
+        smartRefresh.finishRefresh();
     }
 
     @Override
@@ -54,11 +88,38 @@ public class WechatListFragment extends BaseMvpFragment<Contract.WechatlistPrese
     protected void initData() {
         super.initData();
         id = Integer.parseInt(getArguments().getString("id"));
-        mPresenter.initwechatlistPresenter(id,page);
-        wechatlistRec.setLayoutManager(new LinearLayoutManager(getActivity()));
-        DividerItemDecoration divider = new DividerItemDecoration(getActivity(),DividerItemDecoration.VERTICAL);
-        divider.setDrawable(ContextCompat.getDrawable(getActivity(),R.drawable.wechat_shape));
-        wechatlistRec.addItemDecoration(divider);
+        mPresenter.initwechatlistPresenter(id, page);
+        showLoading();
+
 
     }
+
+    @Override
+    protected void initView() {
+
+        wechatlistRec.setLayoutManager(new LinearLayoutManager(getActivity()));
+        wechatListsAdapter = new WechatListsAdapter();
+        wechatlistRec.setAdapter(wechatListsAdapter);
+        DividerItemDecoration divider = new DividerItemDecoration(mContext, DividerItemDecoration.VERTICAL);
+        divider.setDrawable(ContextCompat.getDrawable(mContext, R.drawable.wechat_shape));
+        wechatlistRec.addItemDecoration(divider);
+    }
+
+    @Override
+    protected void initListener() {
+     smartRefresh.setOnRefreshLoadMoreListener(new OnRefreshLoadMoreListener() {
+         @Override
+         public void onLoadMore(@NonNull RefreshLayout refreshLayout) {
+             page++;
+             initData();
+         }
+
+         @Override
+         public void onRefresh(@NonNull RefreshLayout refreshLayout) {
+             page=1;
+             initData();
+         }
+     });
+    }
+
 }
--- app/src/main/java/com/android/wanandroid/module/wechat/wechatadapter/WechatListsAdapter.java
+++ app/src/main/java/com/android/wanandroid/module/wechat/wechatadapter/WechatListsAdapter.java
@@ -11,6 +11,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.wanandroid.R;
 
+import com.android.wanandroid.module.project.entity.ProjectItemData;
 import com.android.wanandroid.module.wechat.WechatListBean;
 
 import java.util.List;
@@ -18,17 +19,23 @@ import java.util.List;
 public class WechatListsAdapter extends RecyclerView.Adapter<WechatListsAdapter.ViewHolder> {
 
     private List<WechatListBean.DatasBean> list;
-    private Context context;
+    public void initData(List<WechatListBean.DatasBean> mProjectLists) {
+        if (list != null) {
+            list.clear();
+        }
+        list = mProjectLists;
+        notifyDataSetChanged();
+    }
 
-    public WechatListsAdapter(List<WechatListBean.DatasBean> list, Context context) {
-        this.list = list;
-        this.context = context;
+    public void addData(List<WechatListBean.DatasBean> mProjectLists) {
+        list.addAll(mProjectLists);
+        notifyDataSetChanged();
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View inflate1 = LayoutInflater.from(context).inflate(R.layout.wechat_item, parent,false);
+        View inflate1 = LayoutInflater.from(parent.getContext()).inflate(R.layout.wechat_item, parent,false);
 //        View inflate = View.inflate(context, R.layout.wechat_item, parent);
         return new ViewHolder(inflate1);
     }
@@ -40,11 +47,17 @@ public class WechatListsAdapter extends RecyclerView.Adapter<WechatListsAdapter.
         holder.wechatlist_author.setText(list.get(position).getAuthor());
         holder.wechatlist_niceDate.setText(list.get(position).getNiceDate());
         holder.wechatlist_superChapterName.setText(list.get(position).getSuperChapterName()+"_"+list.get(position).getAuthor());
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onClickListener.onClick(view,position);
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return list.size();
+        return list != null ? list.size() : 0;
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
@@ -61,5 +74,14 @@ public class WechatListsAdapter extends RecyclerView.Adapter<WechatListsAdapter.
             wechatlist_niceDate = itemView.findViewById(R.id.wechatlist_niceDate);
             wechatlist_superChapterName = itemView.findViewById(R.id.wechatlist_superChapterName);
         }
+    }
+    private OnClickListener onClickListener;
+
+    public void setOnClickListener(OnClickListener onClickListener) {
+        this.onClickListener = onClickListener;
+    }
+
+    public interface OnClickListener{
+        void onClick(View v,int position);
     }
 }
