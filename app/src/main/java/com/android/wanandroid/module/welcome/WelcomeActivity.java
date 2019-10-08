36e9af63842034b9c3dcb5629e5a3da8cf6280e1
wfyw <2905822021@qq.com>
2019-10-08 10:00:09 +0800

    页面优化

--- app/src/main/java/com/android/wanandroid/module/welcome/WelcomeActivity.java
+++ app/src/main/java/com/android/wanandroid/module/welcome/WelcomeActivity.java
@@ -1,14 +1,34 @@
 package com.android.wanandroid.module.welcome;
 
+import android.content.Intent;
+import android.os.CountDownTimer;
+
 import com.android.mymvp.base.BaseActivity;
 import com.android.wanandroid.R;
+import com.android.wanandroid.module.activity.HomeActivity;
+import com.android.wanandroid.test.MainActivity;
 
 public class WelcomeActivity extends BaseActivity {
 
-   @Override
-   public int initLayout() {
-      return R.layout.activity_welcome;
-   }
+    @Override
+    public int initLayout() {
+        return R.layout.activity_welcome;
+    }
 
+    @Override
+    protected void initView() {
+        super.initView();
+        CountDownTimer countDownTimer = new CountDownTimer(3000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+            }
 
+            @Override
+            public void onFinish() {
+                startActivity(new Intent(WelcomeActivity.this, HomeActivity.class));
+                finish();
+            }
+        };
+        countDownTimer.start();
+    }
 }
